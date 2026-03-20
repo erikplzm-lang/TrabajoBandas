00b966da2550caf8218c521d3ef7066c61888ed1
Erik Plaza Morales <erikplzm@gmail.com>
2026-03-20 10:01:42 +0100

    Metodo principal de lanzamiento

---
+++ src/jcolonia/TrabajoMusicos/LanzadorBandas.java
@@ -0,0 +1,11 @@
+package jcolonia.TrabajoMusicos;
+
+/**
+ * Clase lanzadora del proyecto.
+ */
+public class LanzadorBandas {
+    public static void main(String[] args) {
+        ControlBanda programa = new ControlBanda();
+        programa.buclePrincipal();
+    }
+}
